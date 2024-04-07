8f79f70900b91bf5a740fee365ca67e84e05541a
HprogrammerArif <programmerhabluarif@gmail.com>
2024-04-08 04:15:51 +0600

    add and setup folder structure

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,11 @@
+import Header from "../shared/Header/Header";
+import Navbar from "../shared/Navbar/Navbar";
 
 const Home = () => {
   return (
     <div>
+      <Header></Header>
+      <Navbar></Navbar>
       <h2 className="text-3xl font-poppins font-bold">This is home component</h2>
     </div>
   );
---
+++ src/pages/shared/Header/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+  return (
+    <div>
+      <h2 className="text-3xl">Header File</h2>
+    </div>
+  );
+};
+
+export default Header;
