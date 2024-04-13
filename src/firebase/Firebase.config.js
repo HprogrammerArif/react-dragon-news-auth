b38404fd7b910a2ae4235e19ca09a387e70ff2a1
HprogrammerArif <programmerhabluarif@gmail.com>
2024-04-13 08:09:47 +0600

    modyfy config file

--- src/firebase/Firebase.config.js
+++ src/firebase/Firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBPexNYO0KzzT_n8cCciX2NMe4gXGPrGsQ",
-  authDomain: "react-dragon-news-auth-dd628.firebaseapp.com",
-  projectId: "react-dragon-news-auth-dd628",
-  storageBucket: "react-dragon-news-auth-dd628.appspot.com",
-  messagingSenderId: "794376812975",
-  appId: "1:794376812975:web:8328589d4b2eaf1b72975a"
+  apiKey:import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
